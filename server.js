2a982dd20ce4407d292f2b3ff999029d2618e9a9
bulk <harryhamlin@gmail.com>
2023-01-31 21:10:07 -0800

    re added static server to serve css etc

--- server.js
+++ server.js
@@ -2,20 +2,22 @@ const express = require('express');
 const path = require(`path`);
 const PORT = process.env.PORT || 3001;
 const app = express();
-const api = require(`./routes/index.js`);
+const api = require(`./routes/indexRouter.js`);
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.use(`/api`, api);
 
 app.get(`/notes`, (req, res) => {
-    res.sendFile(path.join(__dirname, `./public/notes.html`))
+    res.sendFile(path.join(__dirname, `/public/notes.html`))
 })
 
 app.get(`*`, (req, res) => {
-    res.sendFile(path.join(__dirname, `./public/index.html`))
+    res.sendFile(path.join(__dirname, `/public/index.html`))
 })
 
 app.listen(PORT, () =>
