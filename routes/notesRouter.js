48f5249978bb6ba70849cc7af607f99f1b190111
bulk <harryhamlin@gmail.com>
2023-01-31 22:39:46 -0800

    wrote get function to retrieve stored notes

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,7 +1,21 @@
 const router = require(`express`).Router();
 const db = require(`../db/db.json`);
-const uniqid =  require(`uniqid`);
-const fs = require(`fs`)
+const uniqid = require(`uniqid`);
+const fs = require(`fs`);
+
+router.get(`/:id`, (req, res) => {
+    const requestedNote = parseInt(req.params.id);
+
+    // Iterate through the terms name to check if it matches `req.params.term`
+    for (let i = 0; i < db.length; i++) {
+    if (requestedNote === db[i].id) {
+        return res.json(db[i]);
+    }
+}
+    // Return a message if the term doesn't exist in our DB
+    return res.json('No match found');
+}
+)
 
 router.get(`/`, (req, res) => {
     res.json(db)
@@ -10,19 +24,20 @@ router.get(`/`, (req, res) => {
 
 router.post(`/`, (req, res) => {
     console.log(`${req.method} request received to ${req.method} note`);
-  
+
     const { title, text } = req.body;
-  
-      if (req.body) {
-          const newNote = {
+
+    if (req.body) {
+        const newNote = {
             title,
             text,
             noteId: uniqid()
-          };
-          console.log(`${newNote.title} ${newNote.text} ${newNote.noteId}`)
-      } else {
-          res.error('Error in adding note');
-      }
-  })
+        };
+        res.json(console.log(`${newNote.title} ${newNote.text} ${newNote.noteId}`))
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
 
 module.exports = router
